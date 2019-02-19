67685b7bfb3e014e710329bec4ace30dad785241
Lincy Selvakumari <lincyselvakumari.jeyakumar@microchip.com>
2019-02-19 10:19:38 +0530

    [Touch] Re-build Demo applications MH3-5483

---
+++ apps/button_only_touch_support/qt1_selfcap/firmware/sam_c21_xpro.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# sam_c21_xpro configuration
+CND_ARTIFACT_DIR_sam_c21_xpro=dist/sam_c21_xpro/production
+CND_ARTIFACT_NAME_sam_c21_xpro=sam_c21_xpro.X.production.hex
+CND_ARTIFACT_PATH_sam_c21_xpro=dist/sam_c21_xpro/production/sam_c21_xpro.X.production.hex
+CND_PACKAGE_DIR_sam_c21_xpro=${CND_DISTDIR}/sam_c21_xpro/package
+CND_PACKAGE_NAME_sam_c21_xpro=samc21xpro.x.tar
+CND_PACKAGE_PATH_sam_c21_xpro=${CND_DISTDIR}/sam_c21_xpro/package/samc21xpro.x.tar
