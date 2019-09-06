1a5e7b0b42acc1c0e103cf384ace02d29d3b303f
Dinesh Vedium <dinesh.vedium@microchip.com>
2019-09-06 14:31:44 +0530

    [Touch Help] Touch doc-o-matic help files updated for V3.4.0 Release MH3-21960

--- docs/help_harmony_touch_html_map.h
+++ docs/help_harmony_touch_html_map.h
@@ -1,23 +1,23 @@
 #define IDH_HTML_DRV_Touch_Library                         0x00000000 // DRV Touch Library
 #define IDH_HTML_DRV_Touch_Introduction                    0x00000001 // DRV Touch Introduction
-#define IDH_HTML_DRV_Touch_Clock_Configuration             0x00000002 // DRV Touch Clock_Configuration
-#define IDH_HTML_Clock_Configuration_SAMD20                0x00000003 // Clock_Configuration_SAMD20
-#define IDH_HTML_Clock_Configuration_SAMD21                0x00000004 // Clock_Configuration_SAMD21
-#define IDH_HTML_Clock_Configuration_SAMC20_SAMC21         0x00000005 // Clock_Configuration_SAMC20_SAMC21
-#define IDH_HTML_Clock_Configuration_SAME54_SAME53_SAME51_SAMD51 0x00000006 // Clock_Configuration_SAME54_SAME53_SAME51_SAMD51
-#define IDH_HTML_DRV_Touch_Applications_Help               0x00000007 // DRV Touch Applications Help
-#define IDH_HTML_DRV_Touch_qt1_selfcap                     0x00000008 // DRV Touch qt1_selfcap
-#define IDH_HTML_DRV_Touch_qt1_setlfcap_Building_The_Application 0x00000009 // DRV Touch qt1_setlfcap Building The Application
-#define IDH_HTML_DRV_Touch_qt1_selfcap_MPLAB_Harmony_Configurations 0x0000000A // DRV Touch qt1_selfcap MPLAB Harmony Configurations
-#define IDH_HTML_DRV_Touch_qt1_selfcap_Hardware_Setup      0x0000000B // DRV Touch qt1_selfcap Hardware Setup
-#define IDH_HTML_DRV_Touch_qt1_selfcap_Running_The_Application 0x0000000C // DRV Touch qt1_selfcap Running The Application
-#define IDH_HTML_DRV_Touch_qt1_mutualcap                   0x0000000D // DRV Touch qt1_mutualcap
-#define IDH_HTML_DRV_Touch_qt1_mutualcap_Building_The_Application 0x0000000E // DRV Touch qt1_mutualcap Building The Application
-#define IDH_HTML_DRV_Touch_qt1_mutualcap_MPLAB_Harmony_Configurations 0x0000000F // DRV Touch qt1_mutualcap MPLAB Harmony Configurations
-#define IDH_HTML_DRV_Touch_qt1_mutualcap_Hardware_Setup    0x00000010 // DRV Touch qt1_mutualcap Hardware Setup
-#define IDH_HTML_DRV_Touch_qt1_mutualcap_Running_The_Application 0x00000011 // DRV Touch qt1_mutualcap Running The Application
-#define IDH_HTML_DRV_Touch_on_board_sensor                 0x00000012 // DRV Touch on_board_sensor
-#define IDH_HTML_DRV_Touch_on_board_sensor_Building_The_Application 0x00000013 // DRV Touch on_board_sensor Building The Application
-#define IDH_HTML_DRV_Touch_on_board_sensor_MPLAB_Harmony_Configurations 0x00000014 // DRV Touch on_board_sensor MPLAB Harmony Configurations
-#define IDH_HTML_DRV_Touch_on_board_sensor_Hardware_Setup  0x00000015 // DRV Touch on_board_sensor Hardware Setup
-#define IDH_HTML_DRV_Touch_on_board_sensor_Running_The_Application 0x00000016 // DRV Touch on_board_sensor Running The Application
+#define IDH_HTML_DRV_Touch_Applications_Help               0x00000002 // DRV Touch Applications Help
+#define IDH_HTML_DRV_Touch_qt1_selfcap                     0x00000003 // DRV Touch qt1_selfcap
+#define IDH_HTML_DRV_Touch_qt1_setlfcap_Building_The_Application 0x00000004 // DRV Touch qt1_setlfcap Building The Application
+#define IDH_HTML_DRV_Touch_qt1_selfcap_MPLAB_Harmony_Configurations 0x00000005 // DRV Touch qt1_selfcap MPLAB Harmony Configurations
+#define IDH_HTML_DRV_Touch_qt1_selfcap_Hardware_Setup      0x00000006 // DRV Touch qt1_selfcap Hardware Setup
+#define IDH_HTML_DRV_Touch_qt1_selfcap_Running_The_Application 0x00000007 // DRV Touch qt1_selfcap Running The Application
+#define IDH_HTML_DRV_Touch_qt1_mutualcap                   0x00000008 // DRV Touch qt1_mutualcap
+#define IDH_HTML_DRV_Touch_qt1_mutualcap_Building_The_Application 0x00000009 // DRV Touch qt1_mutualcap Building The Application
+#define IDH_HTML_DRV_Touch_qt1_mutualcap_MPLAB_Harmony_Configurations 0x0000000A // DRV Touch qt1_mutualcap MPLAB Harmony Configurations
+#define IDH_HTML_DRV_Touch_qt1_mutualcap_Hardware_Setup    0x0000000B // DRV Touch qt1_mutualcap Hardware Setup
+#define IDH_HTML_DRV_Touch_qt1_mutualcap_Running_The_Application 0x0000000C // DRV Touch qt1_mutualcap Running The Application
+#define IDH_HTML_DRV_Touch_on_board_sensor                 0x0000000D // DRV Touch on_board_sensor
+#define IDH_HTML_DRV_Touch_on_board_sensor_Building_The_Application 0x0000000E // DRV Touch on_board_sensor Building The Application
+#define IDH_HTML_DRV_Touch_on_board_sensor_MPLAB_Harmony_Configurations 0x0000000F // DRV Touch on_board_sensor MPLAB Harmony Configurations
+#define IDH_HTML_DRV_Touch_on_board_sensor_Hardware_Setup  0x00000010 // DRV Touch on_board_sensor Hardware Setup
+#define IDH_HTML_DRV_Touch_on_board_sensor_Running_The_Application 0x00000011 // DRV Touch on_board_sensor Running The Application
+#define IDH_HTML_app_qt7_self_cap                          0x00000012 // app_qt7_self-cap
+#define IDH_HTML_qt7_self_cap_Building_the_Applications    0x00000013 // qt7_self-cap Building the Applications
+#define IDH_HTML_qt7_self_cap_MPLAB_Harmony_Configurations 0x00000014 // qt7_self-cap MPLAB Harmony Configurations
+#define IDH_HTML_qt7_self_cap_Hardware_Setup               0x00000015 // qt7_self-cap Hardware Setup
+#define IDH_HTML_qt7_self_cap_Running_The_Application      0x00000016 // qt7_self-cap Running The Application
--- docs/help_harmony_touch_html_mapfunc.c
+++ docs/help_harmony_touch_html_mapfunc.c
@@ -7,11 +7,6 @@ int HTML_ID_Mapping(int id)
   {
     case IDH_DRV_Touch_Library                                 : return IDH_HTML_DRV_Touch_Library;
     case IDH_DRV_Touch_Introduction                            : return IDH_HTML_DRV_Touch_Introduction;
-    case IDH_DRV_Touch_Clock_Configuration                     : return IDH_HTML_DRV_Touch_Clock_Configuration;
-    case IDH_Clock_Configuration_SAMD20                        : return IDH_HTML_Clock_Configuration_SAMD20;
-    case IDH_Clock_Configuration_SAMD21                        : return IDH_HTML_Clock_Configuration_SAMD21;
-    case IDH_Clock_Configuration_SAMC20_SAMC21                 : return IDH_HTML_Clock_Configuration_SAMC20_SAMC21;
-    case IDH_Clock_Configuration_SAME54_SAME53_SAME51_SAMD51   : return IDH_HTML_Clock_Configuration_SAME54_SAME53_SAME51_SAMD51;
     case IDH_DRV_Touch_Applications_Help                       : return IDH_HTML_DRV_Touch_Applications_Help;
     case IDH_DRV_Touch_qt1_selfcap                             : return IDH_HTML_DRV_Touch_qt1_selfcap;
     case IDH_DRV_Touch_qt1_setlfcap_Building_The_Application   : return IDH_HTML_DRV_Touch_qt1_setlfcap_Building_The_Application;
@@ -28,6 +23,11 @@ int HTML_ID_Mapping(int id)
     case IDH_DRV_Touch_on_board_sensor_MPLAB_Harmony_Configurations: return IDH_HTML_DRV_Touch_on_board_sensor_MPLAB_Harmony_Configurations;
     case IDH_DRV_Touch_on_board_sensor_Hardware_Setup          : return IDH_HTML_DRV_Touch_on_board_sensor_Hardware_Setup;
     case IDH_DRV_Touch_on_board_sensor_Running_The_Application : return IDH_HTML_DRV_Touch_on_board_sensor_Running_The_Application;
+    case IDH_app_qt7_self_cap                                  : return IDH_HTML_app_qt7_self_cap;
+    case IDH_qt7_self_cap_Building_the_Applications            : return IDH_HTML_qt7_self_cap_Building_the_Applications;
+    case IDH_qt7_self_cap_MPLAB_Harmony_Configurations         : return IDH_HTML_qt7_self_cap_MPLAB_Harmony_Configurations;
+    case IDH_qt7_self_cap_Hardware_Setup                       : return IDH_HTML_qt7_self_cap_Hardware_Setup;
+    case IDH_qt7_self_cap_Running_The_Application              : return IDH_HTML_qt7_self_cap_Running_The_Application;
     default: return -1;
   }
 }
