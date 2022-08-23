cdaa86ac5abc95e79e60749c00c6bc636c806bf9
Kamalakannan Manivasagam <kamalakannan.manivasagam@microchip.com>
2022-08-23 11:31:11 +0530

    [Touch FW] CSD feature for SAML21 is Disabled - MH3-70520

--- config/touch_target_device.py
+++ config/touch_target_device.py
@@ -10,7 +10,7 @@ class classTouchTargetDevice():
         self.xPads = set()
         self.yPads = set()
         self.adc_based_acquisition = set(["SAME54","SAME53","SAME51","SAMD51"])
-        self.no_csd_support = set(["SAMD20","SAMD21","SAMDA1","SAMHA1","SAMD10","SAMD11"])
+        self.no_csd_support = set(["SAMD20","SAMD21","SAMDA1","SAMHA1","SAMD10","SAMD11", "SAML21"])
         self.non_lump_support = set(["PIC32MZW", "PIC32MZDA"])
         self.picDevices = ["PIC32MZW", "PIC32MZDA"]
         self.timer_driven_shield_support = set(["SAMD21","SAMDA1","SAMHA1","SAME54","SAME53","SAME51","SAMD51","SAMC21","SAMC20","SAML21","SAML22","SAMD10","SAMD11","SAMD20"])
